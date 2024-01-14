d3db5287a92fba595bc3d5440a8733622fdfff5c
shoot649854 <jojoto9945@icloud.com>
2024-01-14 11:03:38 -0800

    ✨ Adding profile photo on Header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,10 +5,13 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
+import Box from '@mui/system/Box';
+import Avatar from '@mui/material/Avatar';
 
 import Project from './page/Project';
 import Blog from './Blog';
-import { Box } from '@mui/system';
+
+import profile from '../data/profile/profile.jpeg';
 
 function Header() {
   const navigate: NavigateFunction = useNavigate();
@@ -16,14 +19,17 @@ function Header() {
   return (
     <React.Fragment>
       <Box sx={{ display: 'flex', justifyContent: 'center', backgroundColor: '#fff'}}>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-          
-        <Toolbar component="nav" variant="dense">
-          <Button variant="text" onClick={() => navigate('/blog')}>blog</Button>
-          <Button variant="text" onClick={() => navigate('/project')}>Project</Button>
-        </Toolbar>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+            
+          <Toolbar component="nav" variant="dense">
+            <Button variant="text" onClick={() => navigate('/blog')}>blog</Button>
+            <Button variant="text" onClick={() => navigate('/project')}>Project</Button>
+          </Toolbar>
+        </Box>
+        <Avatar alt="Profile Photo" src={profile} />
       </Box>
 
     </React.Fragment>
