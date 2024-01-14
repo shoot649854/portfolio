cf22dc5fa4c46125ea8ec761a90ee78dd465c656
shoot649854 <jojoto9945@icloud.com>
2024-01-14 09:32:04 -0800

    🦵 Edit on Footer on all pages

--- src/component/page/Project.tsx
+++ src/component/page/Project.tsx
@@ -41,8 +41,11 @@ const Project = () => {
         </Box>
       </Box> */}
 
+      <Typography fontFamily="sans-serif" variant="h1" fontSize="42px" color="black" margin={5}>
+          Project Gallary
+      </Typography>
       <Box display="flex" sx={{ gap: 2 }}>
-        <Link to="/project/1">
+        <Link to="/project/1" style={{ textDecoration: 'none' }}>
           <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
               <CardMedia
@@ -55,7 +58,7 @@ const Project = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   Qiita × Fast DOCTOR Health Tech Hackathon
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="subtitle2" color="text.secondary">
                   Collaborated with Japense university students to create AR App with Flutter that Toyota sponsors. 
                   Utilize Luma API and Google Map API for 3D models and map feature
                 </Typography>
@@ -64,7 +67,7 @@ const Project = () => {
           </Card>
         </Link>
 
-        <Link to="/project/2">
+        <Link to="/project/2"style={{ textDecoration: 'none' }}>
           <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
               <CardMedia
@@ -77,7 +80,7 @@ const Project = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   Electric Sheep Hackathon (Mobility and Big Data)
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="subtitle2" color="text.secondary">
                   Collaborated with Japense university students to create AR App with Flutter that Toyota sponsors. 
                   Utilize Luma API and Google Map API for 3D models and map feature
                 </Typography>
@@ -86,7 +89,7 @@ const Project = () => {
           </Card>
         </Link>
 
-        <Link to="/project/3">
+        <Link to="/project/3"style={{ textDecoration: 'none' }}>
           <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
               <CardMedia
@@ -99,7 +102,7 @@ const Project = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   Symposium Cyberwarfare in Russia and Ukraine
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="subtitle2" color="text.secondary">
                   Researched on cyber-warfare I presented at research project. Worked with technology expert Kenneth Geers and my mentor, professor Alani Mayada.
                 </Typography>
               </CardContent>
