f476202a7b4c806edfbbfba02f8794b5a490e045
shoot649854 <jojoto9945@icloud.com>
2024-01-14 20:22:19 -0800

    ⚒️ Fixing phone window setting for (max-width: 800px)

--- src/Layout/AppLayout.tsx
+++ src/Layout/AppLayout.tsx
@@ -15,17 +15,19 @@ const AppLayout = () => {
         margin={{ top: 0, right: 25, bottom: 0, left: 25 }}
         marginLeft={isExtraSmallScreen ? 1 : 25}
         marginRight={isExtraSmallScreen ? 1 : 25}
+        paddingX={isNarrowScreen ? 1 : 0}
         sx={{
           backgroundColor: "#f5f6f6",
           minHeight: "100vh",
           "@media (max-width: 800px)": {
-            minWidth: isSmallScreen ? "700px" : "auto",
+            minWidth: isSmallScreen ? "430px" : "auto",
           },
           "@media (max-width: 576px)": {
             maxWidth: isExtraSmallScreen ? "200px" : "auto",
           },
           "@media (max-width: 430px)": {
             maxWidth: isNarrowScreen ? "100%" : "auto",
+            padding: isNarrowScreen ? "0 10px" : "0",
           },
         }}
       >
@@ -33,9 +35,9 @@ const AppLayout = () => {
         <Box
           padding={{ top: 3, right: 20, bottom: 10, left: 20 }}
           paddingTop={3}
-          paddingRight={25}
+          paddingRight={isSmallScreen? 2: 25}
           paddingBottom={10}
-          paddingLeft={25}
+          paddingLeft={isSmallScreen? 2: 25}
           sx={{
             borderRadius: "1%",
             flexGrow: 1,
