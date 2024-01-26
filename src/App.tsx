97c6e33bf785c7570f702f6c602c0e455ecb7ee9
shoot649854 <jojoto9945@icloud.com>
2024-01-25 16:16:42 -0800

    💡Use HashRouter to maintain the application state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import AppLayout from "./Layout/AppLayout";
 import Blog from "./page/Homepage";
